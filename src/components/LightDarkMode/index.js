839bef1e167815fb61bd41a9185ee842b4dab6af
NRNPhanideep <phanideep14@gmail.com>
2023-01-05 09:44:44 +0000

    getting indentation error everytime

---
+++ src/components/LightDarkMode/index.js
@@ -0,0 +1,33 @@
+// Write your code here
+import {Component} from 'react'
+import './index.css'
+
+class LightDarkMode extends Component {
+    state = {
+      isDarkMode : true,
+    }
+
+    onClickButton = () => {
+        this.setState(prevState =>({isDarkMode: !prevState.isDarkMode}))
+    }
+
+    render() {
+        const {isDarkMode} = this.setState
+        const anotherClassName = isDarkMode ? 'dark-mode' : 'light-mode'
+        const buttonText = isDarkMode ? 'Light Mode' : 'Dark Mode'
+        return (
+            <div className="bg-container">
+                <div className={`container ${anotherclassName}`}>
+                    <h1 className="heading">Click to Change Mode</h1>
+                    <button className="button" 
+                    onClick={this.onClickButton}
+                    className="button">
+                    {buttonText}
+                    </button>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default LightDarkMode
